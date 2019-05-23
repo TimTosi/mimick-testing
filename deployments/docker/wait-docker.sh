4ae5bacde7ac6c9cab35eca587343554ee8aee84
Tim Tosi <timothee.tosi@gmail.com>
2019-05-23 19:24:54 +0100

    update(docker): use nc instead of pg_isready for wait script

--- deployments/docker/wait-docker.sh
+++ deployments/docker/wait-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for i in `seq 1 20`;
 do
-    pg_isready -h localhost -p 5432 && \
+    nc -z localhost 5432 \
+    # pg_isready -h localhost -p 5432 && \
     echo 'Success' && exit 0
     echo -n .
     sleep 1
